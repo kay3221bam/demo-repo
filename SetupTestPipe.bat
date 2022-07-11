a9638c4014341d9f3ec6253b6320ece2aa6b56d0
kay3221bam <109097105+kay3221bam@users.noreply.github.com>
2022-07-11 12:19:50 -0500

    Add files via upload

---
+++ SetupTestPipe.bat
@@ -0,0 +1,20 @@
+@echo off 
+
+MD Tools
+
+CD Tools
+
+git clone https://github.ms.northgrum.com/ASIL/Python-Cortex-Devices.git Python-Cortex-Devices
+
+curl -OL https://github.ms.northgrum.com/CortexEcosystem/IRIS-Display/releases/download/v4.10.9/IRIS-windows64.exe
+
+git clone https://github.ms.northgrum.com/Automation/portable-py3.7.0.git portable-py3.7.0
+
+CALL portable-py3.7.0\Scripts\activate.bat
+
+python -m pip install git+https://github.ms.northgrum.com/Automation/tdk-core.git
+
+python -m pip install git+https://github.ms.northgrum.com/Automation/tdk-rfcis.git
+
+
+
